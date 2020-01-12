0ec6139116a5c6b6ffcab3379c72dcc4209ae5be
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-01-12 23:58:53 +0100

    fix(sdk): change input and response dto path

--- sources/sdk/sources/app/api/actions.ts
+++ sources/sdk/sources/app/api/actions.ts
@@ -1,7 +1,7 @@
 import { AxiosResponse }            from 'axios';
 import { T721SDK }                  from '../../index';
-import { ActionsSearchInputDto }    from '@app/server/actions/dto/ActionsSearchInput.dto';
-import { ActionsSearchResponseDto } from '@app/server/actions/dto/ActionsSearchResponse.dto';
+import { ActionsSearchInputDto }    from '@app/server/controllers/actionsets/dto/ActionsSearchInput.dto';
+import { ActionsSearchResponseDto } from '@app/server/controllers/actionsets/dto/ActionsSearchResponse.dto';
 
 export async function actionsSearch(
     token: string,
