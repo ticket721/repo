839a947c5929e28297ff23e414890f74911625b9
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-02-05 14:08:40 +0100

    feat: vaultereum tests

--- sources/server/libs/common/src/vaultereum/Vaultereum.service.ts
+++ sources/server/libs/common/src/vaultereum/Vaultereum.service.ts
@@ -1,7 +1,7 @@
 import { Inject, OnModuleInit } from '@nestjs/common';
-import VaultClientBuilder       from 'node-vault';
-import { ShutdownService }      from '@lib/common/shutdown/Shutdown.service';
-import { ServiceResponse }      from '@lib/common/utils/ServiceResponse';
+import VaultClientBuilder from 'node-vault';
+import { ShutdownService } from '@lib/common/shutdown/Shutdown.service';
+import { ServiceResponse } from '@lib/common/utils/ServiceResponse';
 
 export interface VaultereumOptions {
     VAULT_HOST: string;
@@ -14,13 +14,13 @@ export interface VaultereumOptions {
     VAULT_ETHEREUM_NODE_NETWORK_ID: number;
 }
 
-export interface VaultClient {
-    read(path: string, args?: any): Promise<any>;
-    write(path: string, args?: any): Promise<any>;
+export abstract class VaultClient {
+    abstract read(path: string, args?: any): Promise<any>;
+    abstract write(path: string, args?: any): Promise<any>;
 }
 
+// tslint:disable-next-line:max-classes-per-file
 export class VaultereumService implements OnModuleInit {
-
     private client: VaultClient = null;
 
     constructor(
@@ -29,8 +29,13 @@ export class VaultereumService implements OnModuleInit {
         private readonly shutdownService: ShutdownService,
     ) {}
 
-    private async configure(): Promise<VaultClient> {
-        const client = VaultClientBuilder({
+    /* istanbul ignore next */
+    public build(args: any): VaultClient {
+        return VaultClientBuilder(args);
+    }
+
+    public async configure(): Promise<VaultClient> {
+        const client = this.build({
             apiVersion: 'v1',
             endpoint: `${this.vaultereumModuleOptions.VAULT_PROTOCOL}://${this.vaultereumModuleOptions.VAULT_HOST}:${this.vaultereumModuleOptions.VAULT_PORT}`,
             token: this.vaultereumModuleOptions.VAULT_TOKEN,
@@ -61,42 +66,39 @@ export class VaultereumService implements OnModuleInit {
         this.client = await this.configure();
     }
 
-    async write<DataType = any>(path: string, args?: any): Promise<ServiceResponse<DataType>> {
+    async write<DataType = any>(
+        path: string,
+        args?: any,
+    ): Promise<ServiceResponse<DataType>> {
         try {
-
             const res = (await this.client.write(path, args)) as DataType;
             return {
                 error: null,
                 response: res,
             };
-
         } catch (e) {
-
             return {
                 error: e.message,
                 response: null,
             };
-
         }
     }
 
-    async read<DataType = any>(path: string, args?: any): Promise<ServiceResponse<DataType>> {
+    async read<DataType = any>(
+        path: string,
+        args?: any,
+    ): Promise<ServiceResponse<DataType>> {
         try {
-
             const res = (await this.client.read(path, args)) as DataType;
             return {
                 error: null,
                 response: res,
             };
-
         } catch (e) {
-
             return {
                 error: e.message,
                 response: null,
             };
-
         }
     }
-
 }
