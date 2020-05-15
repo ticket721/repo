84857c81dced297b432b7d437b01ddcbb5e2697c
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-05-15 19:35:05 +0200

    feat: elassandra, redis & helmsman configs

--- env/dev_scripts/get_pre_pull_list.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl get pods --all-namespaces -o jsonpath="{..image}" |\
-     tr -s '[[:space:]]' '\n' |\
-     sort |\
-     uniq |\
-     xargs -n 1 echo 'docker pull'
-
