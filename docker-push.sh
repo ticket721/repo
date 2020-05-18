6647b2617c3647c576d268ada9c0a17cddc570de
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-05-18 10:53:39 +0200

    fix: docker push command

--- docker-push.sh
+++ docker-push.sh
@@ -2,12 +2,12 @@
 docker login docker.pkg.github.com --username mortimr --password ${GITHUB_TOKEN}
 
 # Tags
-docker tag server:${TAG} docker.pkg.github.com/phanatic/repo/server:${TAG}
-docker tag worker:${TAG} docker.pkg.github.com/phanatic/repo/worker:${TAG}
+docker tag server:${TAG} docker.pkg.github.com/ticket721/repo/server:${TAG}
+docker tag worker:${TAG} docker.pkg.github.com/ticket721/repo/worker:${TAG}
 
 # Push
-docker push docker.pkg.github.com/phanatic/repo/server:${TAG}
-docker push docker.pkg.github.com/phanatic/repo/worker:${TAG}
+docker push docker.pkg.github.com/ticket721/repo/server:${TAG}
+docker push docker.pkg.github.com/ticket721/repo/worker:${TAG}
 
 
 
