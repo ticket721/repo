65bf327d22993d7870cea5d6f53752e631e9cfd7
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-05-19 15:23:34 +0200

    fix: change dependencies to devDependencies in runner script

--- gulp/utils/check_dependencies.ts
+++ gulp/utils/check_dependencies.ts
@@ -4,12 +4,12 @@ export async function check_dependencies() {
     const package_json = await require(from_root('./package.json'));
 
     if (
-        package_json.dependencies['web3-provider-engine'] !== '15.0.7' ||
-        package_json.dependencies['@ledgerhq/hw-transport-node-hid'] !== '5.13.1' ||
-        package_json.dependencies['@ledgerhq/web3-subprovider'] !== '5.14.0' ||
-        package_json.dependencies['@ledgerhq/hw-app-eth'] !== '5.14.0' ||
-        package_json.dependencies['@ledgerhq/logs'] !== '5.13.1' ||
-        package_json.dependencies['@0x/subproviders'] !== '6.0.8'
+        package_json.devDependencies['web3-provider-engine'] !== '15.0.7' ||
+        package_json.devDependencies['@ledgerhq/hw-transport-node-hid'] !== '5.13.1' ||
+        package_json.devDependencies['@ledgerhq/web3-subprovider'] !== '5.14.0' ||
+        package_json.devDependencies['@ledgerhq/hw-app-eth'] !== '5.14.0' ||
+        package_json.devDependencies['@ledgerhq/logs'] !== '5.13.1' ||
+        package_json.devDependencies['@0x/subproviders'] !== '6.0.8'
     ) {
         console.error(`
             Error detected in package versions required for deployment:
