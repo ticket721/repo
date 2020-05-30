e83f94ed051a7bb4da461b2dbbfde5d835a0988c
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-05-30 22:36:45 +0200

    fix: bash error and job names

--- helmsman.apply.staging.sh
+++ helmsman.apply.staging.sh
@@ -2,7 +2,7 @@
 
 cd modules/@infra_helmsman
 
-env CONTEXT=(kubectl config current-context) \
-CONFIG_STAGING_EFS_ID=(aws efs describe-file-systems --output json | jq '.FileSystems[] | select(.Tags[].Key=="Cluster" and .Tags[].Value=="euw")' | jq '.FileSystemId' | tr -d '"') \
+env CONTEXT=$(kubectl config current-context) \
+CONFIG_STAGING_EFS_ID=$(aws efs describe-file-systems --output json | jq '.FileSystems[] | select(.Tags[].Key=="Cluster" and .Tags[].Value=="euw")' | jq '.FileSystemId' | tr -d '"') \
 TAG=${GITHUB_SHA} \
 helmsman --apply -f ./state.yaml
