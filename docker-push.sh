259d1f25671ce798f4b86d46dc9cec9e7b7ca4b9
Iulian Rotaru <iulian@rotaru.fr>
2020-05-31 23:44:08 +0200

    fix: docker push typo (#580)

--- docker-push.sh
+++ docker-push.sh
@@ -1,7 +1,7 @@
 # Tags
 docker tag server:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
 docker tag worker:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/worker:${TAG}
-docker tag worker:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
+docker tag migrations:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
 
 # Push
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
