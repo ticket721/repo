46f53db1402db72b82c4904abcdea3bc6e88dbff
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-06-09 11:41:19 +0200

    fix: core format error

--- modules/@frontend_core/src/components/EnvValidator/EnvValidator.tsx
+++ modules/@frontend_core/src/components/EnvValidator/EnvValidator.tsx
@@ -1,19 +1,18 @@
 import React, { PropsWithChildren, useState } from 'react';
-import Joi                                    from '@hapi/joi';
+import Joi from '@hapi/joi';
 
 export interface IEnvValidatorInputProps {
     schema: Joi.ObjectSchema;
 }
 
-export const EnvValidator: React.FC<IEnvValidatorInputProps> = (props: PropsWithChildren<IEnvValidatorInputProps>): React.ReactElement => {
-
+export const EnvValidator: React.FC<IEnvValidatorInputProps> = (
+    props: PropsWithChildren<IEnvValidatorInputProps>,
+): React.ReactElement => {
     const [valid, setValidationResult] = useState(null);
 
     if (valid === null) {
         // Validate provided schema
-        const validationResult = props.schema.validate(
-            process.env
-        );
+        const validationResult = props.schema.validate(process.env);
 
         // Override env values with schema validation result
         if (validationResult.value) {
@@ -24,25 +23,20 @@ export const EnvValidator: React.FC<IEnvValidatorInputProps> = (props: PropsWith
 
         // Set state
         setValidationResult(validationResult);
-        return <>{props.children}</>
+        return <>{props.children}</>;
     }
 
     if (valid.error) {
-
-        const errors = valid.error.details.map((errorObject: any): React.ReactNode => (
-            <li style={{color: 'red'}}>
-                {errorObject.message}
-            </li>
-        ));
-        return <div>
-            <h1>An internal configuration error occured</h1>
-            <ul>
-                {errors}
-            </ul>
-        </div>
-
+        const errors = valid.error.details.map(
+            (errorObject: any): React.ReactNode => <li style={{ color: 'red' }}>{errorObject.message}</li>,
+        );
+        return (
+            <div>
+                <h1>An internal configuration error occured</h1>
+                <ul>{errors}</ul>
+            </div>
+        );
     }
 
-    return <>{props.children}</>
-
-}
+    return <>{props.children}</>;
+};
