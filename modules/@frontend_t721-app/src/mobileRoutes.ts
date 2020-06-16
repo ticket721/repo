7c6f553ac0c4b4cb9b884bd00224f164c44e6058
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-06-16 18:36:22 +0200

    feat: start event list on home page
    - Nasty, all in one file, need to be splitted

--- modules/@frontend_t721-app/src/mobileRoutes.ts
+++ modules/@frontend_t721-app/src/mobileRoutes.ts
@@ -37,7 +37,7 @@ export const routes: RouteDatum[] = [
     },
     {
         path: '/home',
-        page: Home,
+        page: NavbarMargin(Home),
         protected: false,
     },
     {
---
+++ modules/@frontend_t721-app/src/screens/Home/locales.ts
@@ -0,0 +1,8 @@
+import i18n from '@frontend/core/lib/utils/i18n'
+
+i18n.addResourceBundle('en', 'home', {
+    browsing_events_in: 'Browsing events in',
+});
+i18n.addResourceBundle('fr', 'home', {
+    browsing_events_in: 'Liste d\'evenements a',
+});
