6edad0a8b49e0c2529d22506d867f35196dea5c2
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-06-16 10:46:31 +0200

    feat: add subspace, balance and profile pages to organizer

--- modules/@frontend_organizer-app/src/index.tsx
+++ modules/@frontend_organizer-app/src/index.tsx
@@ -4,17 +4,21 @@ import './index.css';
 import App                                   from './App';
 import * as serviceWorker                    from './serviceWorker';
 import {Provider}                            from 'react-redux';
-import {Store}                               from 'redux';
-import { AppState, configureStore, history } from '@frontend/core/lib/redux';
+import {Store}                                                                     from 'redux';
+import { AppState, configureStore, history }                                       from '@frontend/core/lib/redux';
 import { organizerInitialState, organizerReducer, organizerSagas, OrganizerState } from './redux/ducks';
-import {BrowserRouter}                       from 'react-router-dom';
-import { ConnectedRouter }                   from 'connected-react-router';
-import { ThemeProvider }                     from 'styled-components';
-import { ScrollToTop }                       from '@frontend/core/lib/components';
-import { GlobalStyles }                      from '@frontend/flib-react/lib/shared';
-import { customThemes }                      from '@frontend/flib-react/lib/config/theme';
-import { EnvValidator }                      from '@frontend/core/lib/components/EnvValidator';
-import { OrganizerAppEnvSchema }             from './utils/env';
+import {BrowserRouter}                                                             from 'react-router-dom';
+import { ConnectedRouter }                                                         from 'connected-react-router';
+import { ThemeProvider }                                                           from 'styled-components';
+import { ScrollToTop }                                                             from '@frontend/core/lib/components';
+import { GlobalStyles }                                                            from '@frontend/flib-react/lib/shared';
+import { customThemes }                                                            from '@frontend/flib-react/lib/config/theme';
+import { EnvValidator }                                                            from '@frontend/core/lib/components/EnvValidator';
+import { OrganizerAppEnvSchema }                                                   from './utils/env';
+import Web3                                                                        from 'web3';
+import { getWeb3 }                                                                 from '@frontend/core/lib/subspace/getWeb3';
+// tslint:disable-next-line:no-var-requires
+const {SubspaceProvider} = require('@embarklabs/subspace-react');
 
 export type MergedAppState = AppState & OrganizerState;
 
@@ -23,21 +27,24 @@ export const store: Store<MergedAppState> = configureStore<OrganizerState>(
     organizerInitialState,
     organizerSagas
 );
+const web3: Web3 = getWeb3();
 
 ReactDOM.render(
     <EnvValidator schema={OrganizerAppEnvSchema}>
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <ThemeProvider theme={customThemes['t721']}>
-                    <GlobalStyles />
-                    <BrowserRouter>
-                        <ScrollToTop>
-                            <App />
-                        </ScrollToTop>
-                    </BrowserRouter>
-                </ThemeProvider>
-            </ConnectedRouter>
-        </Provider>
+        <SubspaceProvider web3={web3}>
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <ThemeProvider theme={customThemes['t721']}>
+                        <GlobalStyles />
+                        <BrowserRouter>
+                            <ScrollToTop>
+                                <App />
+                            </ScrollToTop>
+                        </BrowserRouter>
+                    </ThemeProvider>
+                </ConnectedRouter>
+            </Provider>
+        </SubspaceProvider>
     </EnvValidator>,
     document.getElementById('root')
 );
