2488033010779f53e1c7f22cd09dc55d7a5246f3
Dlpl0 <44698242+TMoynat@users.noreply.github.com>
2020-06-18 14:09:36 +0200

    feat(T721-147): categories count elasticsearch (#695)
    * feat: create categories count route for elasticsearch
    
    * feat: create sdk count method
    
    * test: init categories count test e2e
    
    * chore: format with prettier
    
    * test: categories count route e2e tests
    
    * fix: uncomment tests e2e
    
    * fix: change input type to partial category count sdk
    
    * test: fix input type
    
    * fix: add missing partial in arguments categories sdk
    
    * fix: correct wrong code commentary

---
+++ modules/@backend_nest/apps/server/src/controllers/categories/dto/CategoriesCountInput.dto.ts
@@ -0,0 +1,6 @@
+import { CategoriesSearchInputDto } from '@app/server/controllers/categories/dto/CategoriesSearchInput.dto';
+
+/**
+ * Data model for the categories count query
+ */
+export class CategoriesCountInputDto extends CategoriesSearchInputDto {}
