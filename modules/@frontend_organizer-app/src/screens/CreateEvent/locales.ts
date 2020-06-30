c1a10966ecddd30f3c720e34599cfcb6e3ee002d
Chloé Bourbion <chloebourbion@MacBook-Pro.local>
2020-06-30 18:07:54 +0200

    feat(organizer): add line-break on Create-Event's explanations

--- modules/@frontend_organizer-app/src/screens/CreateEvent/locales.ts
+++ modules/@frontend_organizer-app/src/screens/CreateEvent/locales.ts
@@ -11,9 +11,9 @@ i18n.addResourceBundle('en', 'create_event', {
     general_infos_description: 'Give a name to your event and a concise description for your future attenders. Add tags to improve the referencing of your event.',
     styles_description: 'Set a style for your event and customize your user experience thanks to your "colors signature"',
     dates_description: 'Tell your future participants where and when your event takes place',
-    categories_description: 'Create a Ticket.\nIf you only have one date:\nCreate one or several categories linked to this date\n\nIf you have several dates:\n' +
-    'Create one or several categories linked to multiple dates. If you select "All dates" your categories will be replicated on all previously established dates.' +
-    'Otherwise you can create specific categories for specific dates',
+    categories_description: 'Create a Ticket.\n\nIf you only have one date:\n- Create one or several categories linked to this date\n\nIf you have several dates:\n' +
+    '- Create one or several categories linked to multiple dates. If you select "All dates" your categories will be replicated on all previously established dates.' +
+    '\n\nOtherwise you can create specific categories for specific dates',
 });
 
 i18n.addResourceBundle('fr', 'create_event', {
@@ -27,7 +27,7 @@ i18n.addResourceBundle('fr', 'create_event', {
     general_infos_description: 'Donnez un nom à votre événement et faites une description concise pour vos futurs participants. Ajoutez des tags pour améliorer le référencement de votre événement.',
     styles_description: 'Donnez un style à vos événements et personnalisez l\'expérience de vos utilisateurs grâce à vos "colors signature"',
     dates_description: 'Indiquez à vos futures participants quand, où et à quelle heure votre événement prend place.',
-    categories_description: 'Créez un billet.\nSi vous avez qu\'une seule date:\nCréez une ou plusieurs catégories liée(s) à cette date\n\nSi vous avez plusieurs dates:\n' +
-      'Créez une ou plusieurs catégories liée(s) à plusieurs dates. Si vous sélectionner l\'onglet "toutes les dates" les catégories seront répliqués pour toutes les dates préalablement établies.' +
-      'Sinon vous pouvez faire des catégories spécifiques à certaines dates.',
+    categories_description: 'Créez un billet.\n\nSi vous avez qu\'une seule date:\n- Créez une ou plusieurs catégories liée(s) à cette date\n\nSi vous avez plusieurs dates:\n' +
+      '- Créez une ou plusieurs catégories liée(s) à plusieurs dates. Si vous sélectionner l\'onglet "toutes les dates" les catégories seront répliqués pour toutes les dates préalablement établies.' +
+      '\n\nSinon vous pouvez faire des catégories spécifiques à certaines dates.',
 });
