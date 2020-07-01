1e4f525d7432e7a61f64bdac6ce9b5fb86fcf836
Iulian Rotaru <iulian@rotaru.fr>
2020-07-02 00:50:10 +0200

    feature(t721-198): setup-cli-base (#847)
    * feat: try cache hit optimization
    
    * feat: it's == not =
    
    * feat: it's != not ==
    
    * feat: add cli and basic commands

---
+++ cli/src/run.js
@@ -0,0 +1,28 @@
+const { spawn } = require("child_process");
+
+const run = (command, onEnd) => {
+    return new Promise((ok, ko) => {
+        const split = command.split(' ');
+
+        const prcs = spawn(split[0], split.slice(1));
+
+        prcs.stderr.on('data', (data) => {
+            process.stderr.write(data.toString());
+        });
+
+        prcs.stdout.on('data', (data) => {
+            process.stdout.write(data.toString());
+        });
+
+        prcs.on('exit', (code) => {
+            if (code !== 0) {
+                return ko(code)
+            }
+            ok();
+        });
+    })
+};
+
+module.exports = {
+    run
+};
