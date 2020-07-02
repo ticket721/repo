112241988d61a1fefbc0fd64d4ef9d58b882afc7
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-02 15:47:58 +0200

    feat: setup organizer docker image

--- docker-push.sh
+++ docker-push.sh
@@ -2,11 +2,14 @@
 docker tag server:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
 docker tag worker:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/worker:${TAG}
 docker tag migrations:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
+docker tag organizer:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/organizer:${TAG}
 
 # Push
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/worker:${TAG}
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
+docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/organizer:${TAG}
+
 
 
 
--- modules/@frontend_core/src/subspace/getWeb3.ts
+++ modules/@frontend_core/src/subspace/getWeb3.ts
@@ -1,7 +1,29 @@
 import Web3 from 'web3';
 
+const getHeaders = (): any => {
+    console.log(process.env.REACT_APP_ETHEREUM_HEADERS);
+    if (!process.env.REACT_APP_ETHEREUM_HEADERS) return undefined;
+
+    const headers = JSON.parse(process.env.REACT_APP_ETHEREUM_HEADERS);
+    const ret = [];
+
+    for (const key of Object.keys(headers)) {
+        ret.push({
+            name: key,
+            value: headers[key],
+        });
+    }
+    console.log(ret);
+
+    return ret;
+};
+
 export function getWeb3(): Web3 {
     const nodeUrl = `${process.env.REACT_APP_ETHEREUM_ENDPOINT_PROTOCOL}://${process.env.REACT_APP_ETHEREUM_ENDPOINT_HOST}:${process.env.REACT_APP_ETHEREUM_ENDPOINT_PORT}`;
 
-    return new Web3(new Web3.providers.HttpProvider(nodeUrl));
+    return new Web3(
+        new Web3.providers.HttpProvider(nodeUrl, {
+            headers: getHeaders(),
+        }),
+    );
 }
