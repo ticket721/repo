1316a116efdc6fd221fd8eced3e0177bb3688fdc
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-04 13:08:25 +0200

    ci: set variable if ci should run

--- cli/src/ci/index.js
+++ cli/src/ci/index.js
@@ -3,22 +3,19 @@ const { join } = require('path');
 const { goback, goroot } = require('../root');
 
 const ciexec = async (module, commands) => {
-    const currentBranch = (await runGet(`git symbolic-ref --short HEAD`)).replace('\n', '');
-    console.log(currentBranch);
-
-    if (currentBranch !== 'develop') {
-        const path = goroot();
-        const modulePath = join(process.cwd(), 'modules', module);
-        try {
-            const diff = await runGet(`git diff $(git merge-base develop HEAD)..HEAD ${modulePath}`);
-            goback(path);
-            if (diff === '') {
-                console.log(`No diff found in ${modulePath}`);
-                process.exit(0);
-            }
-        } catch {
-            goback(path)
+    const path = goroot();
+    const modulePath = join(process.cwd(), 'modules', module);
+    try {
+        const diff = await runGet(`git diff $(git merge-base develop HEAD)..HEAD ${modulePath}`);
+        console.log('diff', diff);
+        goback(path);
+        if (diff === '') {
+            console.log(`No diff found in ${modulePath}`);
+            process.exit(0);
         }
+    } catch (e) {
+        console.error(e);
+        goback(path)
     }
 
     await run(commands.join(' '))
