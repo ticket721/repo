301539a48fe54d4f8919b70047401f9e2bd582b3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-06 00:11:18 +0200

    fix: remove useless logs

--- modules/@frontend_core/src/subspace/getWeb3.ts
+++ modules/@frontend_core/src/subspace/getWeb3.ts
@@ -2,7 +2,6 @@ import Web3 from 'web3';
 import { getEnv } from '../utils/getEnv';
 
 const getHeaders = (): any => {
-    console.log(getEnv().REACT_APP_ETHEREUM_HEADERS);
     if (!getEnv().REACT_APP_ETHEREUM_HEADERS) return undefined;
 
     const headers = JSON.parse(getEnv().REACT_APP_ETHEREUM_HEADERS);
@@ -14,13 +13,11 @@ const getHeaders = (): any => {
             value: headers[key],
         });
     }
-    console.log(ret);
 
     return ret;
 };
 
 export function getWeb3(): Web3 {
-    console.log('url', getEnv().REACT_APP_ETHEREUM_URL);
     return new Web3(
         new Web3.providers.HttpProvider(getEnv().REACT_APP_ETHEREUM_URL, {
             headers: getHeaders(),
