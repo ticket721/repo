850f1a83ac79d6581b545239aba896d605100266
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-13 17:13:25 +0200

    feat: final fixes

--- modules/@frontend_t721-app/src/utils/env.ts
+++ modules/@frontend_t721-app/src/utils/env.ts
@@ -4,9 +4,11 @@ import Joi from '@hapi/joi';
 // tslint:disable-next-line:no-empty-interface
 export interface T721AppEnv extends CoreEnv {
     REACT_APP_STRIPE_API_KEY: string;
+    REACT_APP_ETHERSCAN_URL: string;
 }
 
 export const T721AppEnvSchema = (CoreEnvSchema as Joi.ObjectSchema<T721AppEnv>).append({
-    REACT_APP_STRIPE_API_KEY: Joi.string().required()
+    REACT_APP_STRIPE_API_KEY: Joi.string().required(),
+    REACT_APP_ETHERSCAN_URL: Joi.string().required(),
 }).unknown();
 
