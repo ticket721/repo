239a8a132350f26474f97e0b296d69dc7b0f4e12
Jeremy Goldsztejn <jeremy.goldsztejn@gmail.com>
2020-07-16 15:20:13 +0200

    style: make scan page nav more obvious

--- modules/@frontend_staff-app/src/screens/Scan/TopNavbar.tsx
+++ modules/@frontend_staff-app/src/screens/Scan/TopNavbar.tsx
@@ -24,11 +24,11 @@ export const TopNavbar: React.FC<TopNavbarProps> = ({ events, dates }: TopNavbar
         <SafeOffsetContainer>
             <NavbarWrapper>
                 <div onClick={() => history.push('/stats')}>
-                    <Icon icon={'stats'} size={'16px'} color={'rgba(255, 255, 255, 0.9)'} />
+                    <Icon icon={'stats'} size={'22px'} color={'rgba(255, 255, 255, 0.9)'} />
                 </div>
                 <span>{dateName || t('choose_event')}</span>
                 <div onClick={() => history.push('/list')}>
-                    <Icon icon={'attendees'} size={'16px'} color={'rgba(255, 255, 255, 0.9)'} />
+                    <Icon icon={'attendees'} size={'22px'} color={'rgba(255, 255, 255, 0.9)'} />
                 </div>
             </NavbarWrapper>
             <EventSelection events={events} dates={dates}/>
@@ -39,7 +39,7 @@ export const TopNavbar: React.FC<TopNavbarProps> = ({ events, dates }: TopNavbar
 const SafeOffsetContainer = styled.div`
     display: flex;
     flex-direction: column;
-    padding: ${(props) => props.theme.regularSpacing} ${(props) => props.theme.biggerSpacing};
+    padding: ${(props) => props.theme.regularSpacing} ${(props) => props.theme.biggerSpacing} ${(props) => props.theme.smallSpacing};
     position: fixed;
     padding-top: calc(${(props) => props.theme.regularSpacing} + constant(safe-area-inset-top));
     padding-top: calc(${(props) => props.theme.regularSpacing} + env(safe-area-inset-top));
@@ -55,14 +55,14 @@ const NavbarWrapper = styled.div`
     align-items: center;
     font-size: 14px;
     font-weight: 500;
-    margin-bottom: ${props => props.theme.regularSpacing};
+    margin-bottom: ${props => props.theme.smallSpacing};
 
     & > span {
-        font-size: 13px;
+        font-size: 12px;
         font-weight: 500;
         text-overflow: ellipsis;
         white-space: nowrap;
-        margin: 0 ${props => props.theme.smallSpacing};
+        margin: 0 ${props => props.theme.biggerSpacing};
         overflow: hidden;
     }
 `;
