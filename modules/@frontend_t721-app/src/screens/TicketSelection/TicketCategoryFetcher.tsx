1830d9c6a14b8d809e6aaf0acc7934cb68af2974
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-18 12:27:54 +0200

    fix(t721-259): select category title translation

--- modules/@frontend_t721-app/src/screens/TicketSelection/TicketCategoryFetcher.tsx
+++ modules/@frontend_t721-app/src/screens/TicketSelection/TicketCategoryFetcher.tsx
@@ -4,6 +4,7 @@ import { DateEntity }               from '@common/sdk/lib/@backend_nest/libs/com
 import { TicketDateCategoryList }   from './TicketDateCategoryList';
 import { TicketGlobalCategoryList } from './TicketGlobalCategoryList';
 import { TicketSelectionCta }       from './TicketSelectionCta';
+import { useTranslation }           from 'react-i18next';
 
 export interface TicketCategoryFetcherProps {
     date: DateEntity;
@@ -21,9 +22,10 @@ const TicketSelectionTitle = styled.h1`
 export const TicketCategoryFetcher: React.FC<TicketCategoryFetcherProps> = (props: TicketCategoryFetcherProps): JSX.Element => {
 
     const [selection, setSelection] = useState({ section: null, selection: null, category: null });
+    const [t] = useTranslation('event_ticket_list')
 
     return <TicketSelectionContainer>
-        <TicketSelectionTitle>Select Category</TicketSelectionTitle>
+        <TicketSelectionTitle>{t('title')}</TicketSelectionTitle>
         <TicketDateCategoryList date={props.date} selection={selection.selection} section={selection.section} setSelection={setSelection}/>
         <br/>
         <TicketGlobalCategoryList date={props.date} selection={selection.selection} section={selection.section} setSelection={setSelection}/>
