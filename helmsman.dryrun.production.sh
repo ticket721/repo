51f271eb3dec4b8bebf663052e74d03d8e904dba
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-07-26 11:36:54 +0200

    fix: traductions and analytics

--- helmsman.dryrun.production.sh
+++ helmsman.dryrun.production.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd modules/@infra_helmsman
+cd modules/@infra_helmsman-prod
 
 env CONTEXT=$(kubectl config current-context) \
 CONFIG_STAGING_EFS_ID=$(aws efs describe-file-systems --output json | jq '.FileSystems[] | select(.Tags[].Key=="Cluster" and .Tags[].Value=="euw-prod")' | jq '.FileSystemId' | tr -d '"') \
