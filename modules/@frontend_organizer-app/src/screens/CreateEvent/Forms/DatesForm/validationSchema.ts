e825b0aaef067e8c311456b9276418e93da8c8f5
Jeremy Goldsztejn <jeremy.goldsztejn@gmail.com>
2020-07-28 20:47:11 +0200

    feat: merge datepicker and timepicker together

--- modules/@frontend_organizer-app/src/screens/CreateEvent/Forms/DatesForm/validationSchema.ts
+++ modules/@frontend_organizer-app/src/screens/CreateEvent/Forms/DatesForm/validationSchema.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 const eventBegin = yup.date().min(new Date(), 'past_date_forbidden').required('date_required');
-const eventEnd = yup.date().min(new Date(), 'past_date_forbidden').required('date_required');
+const eventEnd = yup.date().min(yup.ref('eventBegin'), 'end_must_be_gt_start').required('date_required');
 const location = yup.object().shape({
         lat: yup.number(),
         lon: yup.number(),
