a65baecfca6c54747fc83fc6592a46e26c558508
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-08-02 21:35:34 +0200

    feat: add script to check if images already exist

---
+++ should_we_build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+   ./find-ecr-image.sh ticket721/server ${TAG} \
+&& ./find-ecr-image.sh ticket721/worker ${TAG} \
+&& ./find-ecr-image.sh ticket721/migrations ${TAG} \
+&& ./find-ecr-image.sh ticket721/organizer ${TAG} \
+&& ./find-ecr-image.sh ticket721/t721app ${TAG} \
+&& ./find-ecr-image.sh ticket721/staff ${TAG} \
+
+if [ $? -eq 1 ]; then
+  echo "::set-output name=should_build::true"
+else
+  echo "::set-output name=should_build::false"
+fi
