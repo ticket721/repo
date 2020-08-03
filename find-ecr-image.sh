729ced7df8bb76a186e6321d880f653c26e2c9d1
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-08-03 11:53:06 +0200

    fix: add ecr image detection verbosity

--- find-ecr-image.sh
+++ find-ecr-image.sh
@@ -5,12 +5,13 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
-IMAGE_META="$( aws ecr describe-images --output=json --repository-name=$1 --image-ids=imageTag=$2 2> /dev/null )"
+IMAGE_META="$( aws ecr describe-images --output=json --repository-name=$1 --image-ids=imageTag=$2  )"
 
 if [[ $? == 0 ]]; then
     IMAGE_TAGS="$( echo ${IMAGE_META} | jq '.imageDetails[0].imageTags[0]' -r )"
     echo "$1:$2 found"
 else
+    echo "$IMAGE_META"
     echo "$1:$2 not found"
     exit 1
 fi
