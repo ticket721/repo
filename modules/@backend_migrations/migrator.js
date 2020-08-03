a53e5e0643db068783646dc7360719bb7f289427
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-08-03 17:04:27 +0200

    feat: create generic migration runner

---
+++ modules/@backend_migrations/migrator.js
@@ -0,0 +1,78 @@
+const config = require('./config');
+const spawnSync = require('child_process').spawnSync;
+
+const direction = process.argv[2];
+
+if (['up', 'down'].indexOf(direction) === -1) {
+    console.error(`Usage: node migrator.js (up/down)`);
+    process.exit(1);
+}
+
+const checkCall = (res) => {
+    if (res.status !== 0) {
+        console.error(`\tProcess exited with code ${res.status}`);
+
+        console.error('\twith following error logs');
+        console.error('==========>');
+        console.error();
+        console.error(res.output[2].toString());
+        console.error();
+        console.error('<==========');
+        console.error('\twith following logs');
+        console.error('==========>');
+        console.error();
+        console.error(res.output[1].toString());
+        console.error();
+        console.error('<==========');
+
+        process.exit(res.status);
+    }
+};
+
+let command = null;
+let tasks = null;
+
+switch (direction) {
+    case 'up': {
+
+        console.log('running [cassandra setup]');
+        const cassandraSetup = spawnSync('node', ['keyspace.js'], {
+            cwd: './cassandra'
+        });
+        checkCall(cassandraSetup);
+        
+        console.log('running [elasticsearch setup]');
+        const elasticsearchSetup = spawnSync('npx', ['elastic-migrate', 'setup']);
+        checkCall(elasticsearchSetup);
+
+        command = 'up';
+        tasks = config;
+
+        break ;
+    }
+
+    case 'down': {
+
+        command = 'down';
+        tasks = config.reverse();
+
+        break ;
+    }
+}
+
+for (const task of tasks) {
+    console.log();
+    process.stdout.write(`running [${task.type}][${task.name}][${task.timestamp}][${command}] `);
+    const taskRes = spawnSync('npm', ['run', `${task.type}-migrate-${command}`, task.timestamp], {
+        cwd: task.type
+    });
+    checkCall(taskRes);
+    if (process.env.VERBOSE) {
+        console.log();
+        console.log(taskRes.output[1].toString());
+        console.log();
+    }
+    process.stdout.write(`[OK]\n`);
+}
+console.log();
+console.log(`${command} migration complete`);
