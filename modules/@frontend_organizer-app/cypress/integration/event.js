6bc7aa3d7b6b4403b32680998a360d859a034838
Chloé Bourbion <chloebourbion@MacBook-Pro.local>
2020-08-04 10:47:05 +0200

    feat(organizer): add update form test

--- modules/@frontend_organizer-app/cypress/integration/event.js
+++ modules/@frontend_organizer-app/cypress/integration/event.js
@@ -1,8 +1,8 @@
-import { me, user } from '../data/user';
+import { me } from '../data/user';
 import { rightsResponse } from '../data/rightsResponse';
 import { eventsResponse } from '../data/eventsResponse';
 import { categoriesResponse } from '../data/categoriesResponse';
-import { datesResponse } from '../data/datesResponse';
+import { datesResponse, avatar } from '../data/datesResponse';
 
 const messages = {
     preview: 'User Preview',
@@ -11,12 +11,46 @@ const messages = {
 describe('Event Page', () => {
     beforeEach(() => {
         cy.server();
-        cy.route('GET', 'users/me', me).as('me');
         cy.login();
-        cy.fetchEvent();
+        cy.route('POST', 'rights/search', rightsResponse.event).as('event-right');
+        cy.route('POST', 'events/search', eventsResponse).as('events-search');
+        cy.route('POST', 'dates/search', datesResponse).as('dates-search');
+        cy.route('POST', 'categories/search', categoriesResponse).as('categories-search');
+        cy.route('GET', 'users/me', me).as('me');
     });
     it('successfully loads', () => {
+        cy.visit(`/group/${eventsResponse.events[0].id}/date/${eventsResponse.events[0].dates[0]}`);
         cy.get('h2').should('contain', eventsResponse.events[0].name);
         cy.get('span').should('contain', messages.preview);
     })
+
+    it('general info loads', () => {
+        cy.visit(`/group/${eventsResponse.events[0].id}/date/${eventsResponse.events[0].dates[0]}/general-infos`);
+
+        cy.get(`input[value="${eventsResponse.events[0].name}"]`).should('exist');
+        cy.get('textarea[name=description]').should('contain', datesResponse.dates[0].metadata.description);
+        cy.get('div[id=tags]').within(($tags) => {
+            datesResponse.dates[0].metadata.tags.forEach(tag => {
+                cy.get('div').should('contain', tag);
+            })
+        });
+        cy.get('button[name="Save Changes"]').should('be.disabled');
+    })
+
+    // it('styles loads', () => {
+    //     cy.visit(`/group/${eventsResponse.events[0].id}/date/${eventsResponse.events[0].dates[0]}/styles`);
+    //
+    //     cy.route('GET', `static/${datesResponse.dates[0].metadata.avatar}`, avatar).as('get-avatar');
+    //     cy.get('span').contains(datesResponse.dates[0].metadata.signature_colors[0]);
+    //     cy.get('span').contains(datesResponse.dates[0].metadata.signature_colors[1]);
+    //     cy.get('button[name="Save Changes"]').should('be.disabled');
+    // })
+
+    it('location loads', () => {
+        cy.visit(`/group/${eventsResponse.events[0].id}/date/${eventsResponse.events[0].dates[0]}/location`);
+
+        cy.get(`input[value="${datesResponse.dates[0].location.location_label}"]`).should('exist');
+        cy.get('button[name="Save Changes"]').should('be.disabled');
+    })
+
 })
--- modules/@frontend_organizer-app/cypress/support/commands.js
+++ modules/@frontend_organizer-app/cypress/support/commands.js
@@ -1,4 +1,4 @@
-import { user } from '../data/user';
+import {me, user} from '../data/user';
 import { loginResponse } from '../data/authResponse';
 import {rightsResponse} from "../data/rightsResponse";
 import {eventsResponse} from "../data/eventsResponse";
@@ -8,29 +8,13 @@ import {categoriesResponse} from "../data/categoriesResponse";
 Cypress.Commands.add('login', () => {
     cy.server();
     cy.route('POST', 'authentication/local/login', loginResponse['200']).as('success');
+    cy.route('GET', 'users/me', me).as('me');
 
-    const { email, password, username } = user;
+    const { email, password } = user;
 
     cy.visit('/login');
     cy.get('input[name=email]').type(email);
     cy.get('input[name=password]').type(`${password}{enter}`);
 
     cy.url().should('be', '/');
-});
-
-Cypress.Commands.add('fetchEvent', () => {
-    cy.server();
-    cy.route('POST', 'rights/search', rightsResponse.event).as('event-right');
-    cy.route('POST', 'events/search', eventsResponse).as('events-search');
-    cy.route('POST', 'dates/search', datesResponse).as('dates-search');
-    cy.route('POST', 'categories/search', categoriesResponse).as('categories-search');
-
-    cy.wait('@events-search').then(xhrE => {
-        cy.wait('@dates-search').then(xhrD => {
-            cy.wait('@categories-search').then(xhrC => {
-                cy.get('span').contains(eventsResponse.events[0].name).click();
-            });
-        });
-    });
-    cy.url().should('be', `group/${eventsResponse.events[0].id}/date/${eventsResponse.events[0].dates[0]}`);
 });
