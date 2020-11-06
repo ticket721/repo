435834e3093d117a8ec763988c925195cc5776cb
Dlpl0 <thibault.moynat@epitech.eu>
2020-11-06 11:57:03 +0100

    fix: define custom types in elastic migration

--- modules/@backend_migrations/elasticsearch/migrations/20201105101935_add_venmas_state.js
+++ modules/@backend_migrations/elasticsearch/migrations/20201105101935_add_venmas_state.js
@@ -5,7 +5,31 @@ class M20201105101935_add_venmas_state extends ElasticMigration {
         await this.createIndex('ticket721_venmas_map', 'ticket721');
         await this.putMapping('ticket721_venmas_map', 'venmas_map', {
             "venmas_map": {
-                "discover": ".*"
+                "discover": "^((?!(sections)).*)",
+                "properties": {
+                    "sections": {
+                        "type": "nested",
+                        "cql_collection": "list",
+                        "properties": {
+                            "id": {
+                                "cql_collection": "singleton",
+                                "type": "integer"
+                            },
+                            "type": {
+                                "cql_collection": "singleton",
+                                "type": "text"
+                            },
+                            "owner": {
+                                "cql_collection": "singleton",
+                                "type": "text"
+                            },
+                            "points": {
+                                "cql_collection": "set",
+                                "type": "float"
+                            }
+                        }
+                    }
+                }
             }
         });
         await this.putSettings('ticket721_venmas_map',
