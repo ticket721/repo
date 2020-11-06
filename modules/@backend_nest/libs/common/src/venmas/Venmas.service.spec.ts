5fc96e7660a2447e4b1837e4d2846bd90bbf59cb
Dlpl0 <thibault.moynat@epitech.eu>
2020-11-06 20:28:32 +0100

    chore: reformat code

--- modules/@backend_nest/libs/common/src/venmas/Venmas.service.spec.ts
+++ modules/@backend_nest/libs/common/src/venmas/Venmas.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VenmasService } from './Venmas.service';
 
 describe('VenmasService', () => {
-  let service: VenmasService;
+    let service: VenmasService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [VenmasService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [VenmasService],
+        }).compile();
 
-    service = module.get<VenmasService>(VenmasService);
-  });
+        service = module.get<VenmasService>(VenmasService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
