803dd11fd20f20d56e6bb941f07701f0fead06f6
Dlpl0 <44698242+TMoynat@users.noreply.github.com>
2020-11-06 20:26:37 +0100

    feat(T721-331): create venmas migrations (#1178)
    * feat: add elassandra migration for venmas state
    
    * fix: define custom types in elastic migration
    
    * fix: update cassandra type and table to match our need
    
    * fix: drop table before drop type

---
+++ modules/@backend_migrations/cassandra/1604571522_add_venmas_state.js
@@ -0,0 +1,67 @@
+
+var migration1604571522 = {
+    up : async function (db, handler) {
+        const venmas_section_type_creation = {
+            query: `CREATE TYPE IF NOT EXISTS ticket721.venmas_section (
+                    id UUID,
+                    type text,
+                    name text,
+                    description text,
+                    points list<frozen<tuple<float, float>>>
+                );`,
+            params: []
+        };
+
+        const venmas_map_table_creation = {
+            query: `CREATE TABLE IF NOT EXISTS ticket721.venmas_map (
+                    id UUID PRIMARY KEY,
+                    name text,
+                    owner UUID,
+                    map text,
+                    sections list<frozen<ticket721.venmas_section>>
+                    created_at timestamp,
+                    updated_at timestamp,
+                );`,
+            params: []
+        };
+
+        try {
+
+            console.log('Venmas Section Type Creation');
+            await db.execute(venmas_section_type_creation.query, venmas_section_type_creation.params, { prepare: true });
+
+            console.log('Venmas Map Table Creation');
+            await db.execute(venmas_map_table_creation.query, venmas_map_table_creation.params, { prepare: true });
+
+        } catch (e) {
+            return handler(e, false);
+        }
+        handler(false, true);
+  },
+  down : async function (db, handler) {
+      const venmas_section_type_creation = {
+          query: `DROP TYPE ticket721.venmas_section;`,
+          params: []
+      };
+
+      const venmas_map_table_creation = {
+          query: `DROP TABLE ticket721.venmas_map`,
+          params: []
+      };
+
+      try {
+
+          console.log('Venmas Map Table Creation');
+          await db.execute(venmas_map_table_creation.query, venmas_map_table_creation.params, { prepare: true });
+
+          console.log('Venmas Section Type Creation');
+          await db.execute(venmas_section_type_creation.query, venmas_section_type_creation.params, { prepare: true });
+
+      } catch (e) {
+          return handler(e, false);
+      }
+
+      handler(false, true);
+  }
+};
+module.exports = migration1604571522;
