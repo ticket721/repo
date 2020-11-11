90a285c2c109e10b56139d985ea92c16ed9ecd86
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-11-11 11:15:24 +0100

    fix: remove worker forgotten sections

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,5 @@
 # Tags
 docker tag server:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
-docker tag worker:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/worker:${TAG}
 docker tag migrations:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
 docker tag organizer:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/organizer:${TAG}
 docker tag t721app:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/t721app:${TAG}
@@ -8,7 +7,6 @@ docker tag staff:${TAG} ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticke
 
 # Push
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/server:${TAG}
-docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/worker:${TAG}
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/migrations:${TAG}
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/organizer:${TAG}
 docker push ${AWS_USER_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ticket721/t721app:${TAG}
