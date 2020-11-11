7959041446bb086cfae75b336e4a04904e996ec3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-11-11 21:52:57 +0100

    feat: remove unused etherscan url

--- modules/@frontend_t721-app/src/utils/env.ts
+++ modules/@frontend_t721-app/src/utils/env.ts
@@ -3,14 +3,12 @@ import Joi from '@hapi/joi';
 
 export interface T721AppEnv extends CoreEnv {
     REACT_APP_STRIPE_API_KEY: string;
-    REACT_APP_ETHERSCAN_URL: string;
     REACT_APP_APPSTORE_LINK: string;
     REACT_APP_PLAYSTORE_LINK: string;
 }
 
 export const T721AppEnvSchema = (CoreEnvSchema as Joi.ObjectSchema<T721AppEnv>).append({
     REACT_APP_STRIPE_API_KEY: Joi.string().required(),
-    REACT_APP_ETHERSCAN_URL: Joi.string().required(),
     REACT_APP_APPSTORE_LINK: Joi.string().required(),
     REACT_APP_PLAYSTORE_LINK: Joi.string().required()
 }).unknown();
