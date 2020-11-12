97ee1327e60a1f2d1f5069fba9f60bbd9d524321
dlpl0 <thibault.moynat@epitech.eu>
2020-11-12 13:54:08 +0100

    test: add VenmasEntity tests

--- modules/@backend_nest/libs/common/src/venmas/Venmas.repository.ts
+++ modules/@backend_nest/libs/common/src/venmas/Venmas.repository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from '@iaminfinity/express-cassandra';
 import { VenmasEntity } from '@lib/common/venmas/entities/Venmas.entity';
 
 /**
- * Repository of the RightEntity
+ * Repository of the VenmasEntity
  */
 @EntityRepository(VenmasEntity)
 export class VenmasRepository extends Repository<VenmasEntity> {}
