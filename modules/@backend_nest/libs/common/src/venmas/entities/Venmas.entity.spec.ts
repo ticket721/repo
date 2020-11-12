3a933000b18e66d27e3e7d20282d753d617cdfab
dlpl0 <thibault.moynat@epitech.eu>
2020-11-12 13:54:08 +0100

    test: add VenmasEntity tests

--- modules/@backend_nest/libs/common/src/venmas/entities/Venmas.entity.spec.ts
+++ modules/@backend_nest/libs/common/src/venmas/entities/Venmas.entity.spec.ts
@@ -1,4 +1,4 @@
-import { VenmasEntity } from '@lib/common/venmas/entities/Venmas.entity';
+import { VenmasEntity, Sections } from '@lib/common/venmas/entities/Venmas.entity';
 
 describe('Date Entity', function() {
     describe('constructor', function() {
@@ -6,6 +6,54 @@ describe('Date Entity', function() {
             const venmasEntity = new VenmasEntity();
 
             expect(venmasEntity).toEqual({});
+        });
+
+        it('should build from venmas', function() {
+            const section: Sections = {
+                id: 'ab',
+                type: 'ab',
+                name: 'ab',
+                description: 'ab',
+                points: [[12, 12]],
+            };
+
+            const rawVenmasEntity: VenmasEntity = {
+                id: 'abcd',
+                name: 'abcd',
+                owner: 'abcd',
+                map: 'abcd',
+                sections: section,
+                created_at: new Date(),
+                updated_at: new Date(),
+            };
+
+            const venmas = new VenmasEntity(rawVenmasEntity);
+
+            expect(venmas).toEqual(rawVenmasEntity);
+        });
+
+        it('should build from venmas without id', function() {
+            const section: Sections = {
+                id: 'ab',
+                type: 'ab',
+                name: 'ab',
+                description: 'ab',
+                points: [[12, 12]],
+            };
+
+            const rawVenmasEntity: VenmasEntity = {
+                id: null,
+                name: 'abcd',
+                owner: 'abcd',
+                map: 'abcd',
+                sections: section,
+                created_at: new Date(),
+                updated_at: new Date(),
+            };
+
+            const venmas = new VenmasEntity(rawVenmasEntity);
+
+            expect(venmas).toEqual(rawVenmasEntity);
         });
     });
 });
