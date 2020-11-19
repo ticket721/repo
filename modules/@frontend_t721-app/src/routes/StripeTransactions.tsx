ed2864641c7e7983383510241f812400010ed3ea
Iulian Rotaru <iulian@rotaru.fr>
2020-11-19 11:06:27 +0100

    fix: add bottom margin wrapper (#1203)

--- modules/@frontend_t721-app/src/routes/StripeTransactions.tsx
+++ modules/@frontend_t721-app/src/routes/StripeTransactions.tsx
@@ -1,5 +1,6 @@
 import { InvisibleStatusBarMargin } from '@frontend/core/lib/utils/InvisibleStatusBarMargin';
 import { TopNavMargin }             from '@frontend/core/lib/utils/TopNavMargin';
 import { StripeTransactions }       from '@frontend/core/lib/components/StripeSetup';
+import { BottomMargin }             from '@frontend/core/lib/utils/BottomMargin';
 
-export default InvisibleStatusBarMargin(TopNavMargin(StripeTransactions));
+export default InvisibleStatusBarMargin(TopNavMargin(BottomMargin(StripeTransactions)));
