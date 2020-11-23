bda6a9e1c9eaab28166753b70f5f0cdf64b5fbf8
Goldiggy <33359873+Goldiggy@users.noreply.github.com>
2020-11-23 03:52:01 +0100

    Feat(t721-313): dates categories preview live toggles (#1211)
    * refactor: make account drawer generic
    
    - change for useSelector to useToken in missing places
    
    * refactor: remove unused flib components
    
    * refactor: change app to one entry point
    
    * feat: make search screen responsive
    
    * style: center "no entity" message and CTAs on dashboards
    
    * feat: add richtext + responsive user app
    
    - add richtext component with several features:
      bold, italic, strikethrough, headings, links, images, divider, notices
    
    - make event page and cart responsive for desktop
    
    * feat: continue responsive integration
    
    - finish responsive on t721 app
    - make some arrangement on flib components and organizer
    - css fixes
    - animation improvements
    
    * fix: linter issues and format
    
    * feat: add preview/live toggles
    
    - add toggles to set dates and categories to preview or live

--- modules/@frontend_organizer-app/src/screens/Dashboards/DatesDashboard/DateCard.tsx
+++ modules/@frontend_organizer-app/src/screens/Dashboards/DatesDashboard/DateCard.tsx
@@ -1,6 +1,6 @@
 import { useRequest } from '@frontend/core/lib/hooks/useRequest';
 import { useToken } from '@frontend/core/lib/hooks/useToken';
-import { FullPageLoading, Error } from '@frontend/flib-react/lib/components';
+import { FullPageLoading, Error, Toggle } from '@frontend/flib-react/lib/components';
 import React, { useEffect, useState } from 'react';
 import { v4 } from 'uuid';
 import { useTranslation }  from 'react-i18next';
@@ -9,25 +9,46 @@ import { useHistory, useParams } from 'react-router';
 import { CategoriesSearchResponseDto } from '@common/sdk/lib/@backend_nest/apps/server/src/controllers/categories/dto/CategoriesSearchResponse.dto';
 import { eventParam } from '../../types';
 import { OnlineTag } from '../../../components/OnlineTag';
+import { useLazyRequest } from '@frontend/core/lib/hooks/useLazyRequest';
+import { EventsStatusResponseDto } from '@common/sdk/lib/@backend_nest/apps/server/src/controllers/events/dto/EventsStatusResponse.dto';
+import { useDispatch } from 'react-redux';
+import { PushNotification } from '@frontend/core/lib/redux/ducks/notifications';
 
 export interface DateCardProps {
     id: string;
+    eventStatus: 'preview' | 'live';
+    status: 'preview' | 'live';
     name: string;
     online: boolean;
     avatar: string;
-    primaryColor: string;
+    colors: string[];
     categoryIds: string[];
+    forceRefresh: () => void;
 }
 
-export const DateCard: React.FC<DateCardProps> = ({ id, name, online, avatar, primaryColor, categoryIds }) => {
+export const DateCard: React.FC<DateCardProps> = ({
+    id,
+    eventStatus,
+    status,
+    name,
+    online,
+    avatar,
+    colors,
+    categoryIds,
+    forceRefresh,
+}) => {
     const [ t ] = useTranslation('dates_dashboard');
     const token = useToken();
 
     const history = useHistory();
+    const dispatch = useDispatch();
 
     const { eventId } = useParams<eventParam>();
 
-    const [fetchCategoriesUuid] = useState(v4() + '@dates-fetch');
+    const [fetchCategoriesUuid] = useState(v4() + '@categories-fetch');
+    const [toggleDateStatusUuid] = useState(v4() + '@date-status-toggle');
+
+    const [ dateStatusChanging, setDateStatusChanging ] = useState<boolean>(false);
 
     const [ categoriesInfos, setCategoriesInfos ] = useState<{ startPrice: number, totalSeats: number }>();
 
@@ -48,6 +69,27 @@ export const DateCard: React.FC<DateCardProps> = ({ id, name, online, avatar, pr
         refreshRate: 50,
     }, fetchCategoriesUuid);
 
+    const { response: toggleDateStatusResp, lazyRequest: toggleDateStatus } =
+        useLazyRequest<EventsStatusResponseDto>('events.status', toggleDateStatusUuid);
+
+    const handleLiveToggle = (checked: boolean) => {
+        if (checked === (status === 'live')) {
+            dispatch(PushNotification(t('status_already_in_state', { status }), 'error'));
+            return;
+        }
+
+        setDateStatusChanging(true);
+        toggleDateStatus([
+            token,
+            eventId,
+            {
+                dates: {
+                    [id]: checked,
+                },
+            }
+        ], { force: true });
+    }
+
     useEffect(() => {
         if (categoriesResp.data?.categories.length > 0) {
             const totalSeats = categoriesResp.data.categories.reduce((acc, category) => acc + category.seats, 0);
@@ -59,6 +101,22 @@ export const DateCard: React.FC<DateCardProps> = ({ id, name, online, avatar, pr
     // eslint-disable-next-line
     }, [categoriesResp.data?.categories]);
 
+    useEffect(() => {
+        if (toggleDateStatusResp.data) {
+            forceRefresh();
+            setDateStatusChanging(false);
+        }
+        // eslint-disable-next-line
+    }, [toggleDateStatusResp.data]);
+
+    useEffect(() => {
+        if (toggleDateStatusResp.error) {
+            dispatch(PushNotification(t('toggle_error'), 'error'));
+            setDateStatusChanging(false);
+        }
+        // eslint-disable-next-line
+    }, [toggleDateStatusResp.error]);
+
     if (categoriesResp.loading) {
         return <FullPageLoading/>;
     }
@@ -67,32 +125,64 @@ export const DateCard: React.FC<DateCardProps> = ({ id, name, online, avatar, pr
         return <Error message={t('categories_fetch_error')} onRefresh={forceCategories}/>;
     }
 
-    return <DateCardContainer
-    cover={avatar}
-    onClick={() => history.push(`/event/${eventId}/date/${id}/dates-typology`)}>
-            <Filter/>
-            {
-                online ?
-                <Online><OnlineTag/></Online> :
-                null
-            }
-            <Name>{name}</Name>
-            {
-                categoriesResp.data.categories.length > 0 ?
-                <CategoriesInfos primaryColor={primaryColor}>
-                    <strong>{categoriesInfos?.totalSeats}</strong>&nbsp;{t('seats')}&nbsp;•&nbsp;
-                    {
-                        categoriesInfos?.startPrice > 0 ?
-                        <>{t('from')}&nbsp;<strong>{categoriesInfos.startPrice/100}€</strong></> :
-                        <strong>{t('free')}</strong>
-                    }
-                </CategoriesInfos> :
-                null
-            }
-    </DateCardContainer>;
+    return <Container>
+        {
+            eventStatus === 'live' ?
+            <LiveToggle disabled={dateStatusChanging}>
+                <Toggle
+                name={'live-toggle' + id}
+                label={t(status)}
+                checked={status === 'live'}
+                gradient={colors}
+                disabled={dateStatusChanging}
+                onChange={handleLiveToggle}/>
+            </LiveToggle> :
+            null
+        }
+        <DateCardContainer
+        cover={avatar}
+        live={status === 'live'}
+        onClick={() => history.push(`/event/${eventId}/date/${id}/dates-typology`)}>
+                <Filter/>
+                {
+                    online ?
+                    <Online><OnlineTag/></Online> :
+                    null
+                }
+                <Name>{name}</Name>
+                {
+                    categoriesResp.data.categories.length > 0 ?
+                    <CategoriesInfos primaryColor={colors[0]}>
+                        <strong>{categoriesInfos?.totalSeats}</strong>&nbsp;{t('seats')}&nbsp;•&nbsp;
+                        {
+                            categoriesInfos?.startPrice > 0 ?
+                            <>{t('from')}&nbsp;<strong>{categoriesInfos.startPrice/100}€</strong></> :
+                            <strong>{t('free')}</strong>
+                        }
+                    </CategoriesInfos> :
+                    null
+                }
+        </DateCardContainer>
+    </Container>;
 };
 
-const DateCardContainer = styled.div<{ cover: string }>`
+const Container = styled.div`
+    position: relative;
+`;
+
+const LiveToggle = styled.div<{ disabled: boolean }>`
+    position: absolute;
+    top: -${props => props.theme.doubleSpacing};
+    right: 4px;
+    pointer-events: ${props => props.disabled ? 'none' : null};
+    filter: ${props => props.disabled ? 'greyscale(1)' : null};
+
+    label {
+        padding-top: 3px;
+    }
+`;
+
+const DateCardContainer = styled.div<{ cover: string, live: boolean }>`
     position: relative;
     display: flex;
     flex-direction: column;
@@ -105,6 +195,8 @@ const DateCardContainer = styled.div<{ cover: string }>`
     padding: ${props => props.theme.biggerSpacing};
     border-radius: ${props => props.theme.defaultRadius};
     cursor: pointer;
+
+    filter: ${props => props.live ? null : 'grayscale(0.5)'};
 
     @media screen and (max-width: 1500px) {
         width: calc((100vw - 450px - ${props => props.theme.doubleSpacing}) / 2);
