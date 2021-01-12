d220503d2d32dc3997061f5727e27c0140b6d047
Jeremy Goldsztejn <jeremy.goldsztejn@gmail.com>
2021-01-12 17:53:05 +0100

    ci: add common_global on flib prebuild

--- modules/@common_global/sources/index.ts
+++ modules/@common_global/sources/index.ts
@@ -14,7 +14,7 @@ export { uuidEq, toB32, leftPad, serialize, toHex, decimalToHex, isFutureDateRan
 export { isTransactionHash, toAcceptedTransactionHashFormat, isTrackingId }                                  from './transactions';
 export { getT721ControllerGroupID, encode }                                                                  from './abi';
 export { b64Encode, b64Decode }                                                                              from './encoding';
-export { currencies, symbolOf, fromAtomicValue, format, getAtomicValue }                                     from './currency';
+export { currencies, symbolOf, fromAtomicValue, format, getAtomicValue, getDecimalScale }                                     from './currency';
 export {
     checkEvent,
     EventCreationPayload,
